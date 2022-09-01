1da9e48461f4b09f6595de4d46dcb2a733abc1c3
SuperJu5tin <superjuslib@gmail.com>
2022-08-31 18:06:49 -0600

    added vs and got snake moving

--- snake.py
+++ snake.py
@@ -1,6 +1,11 @@
 import pygame
 pygame.init()
-dis=pygame.display.set_mode((800,300))
+
+dis_width = 300
+dis_height = 300
+
+game = pygame.display.set_mode((dis_width, dis_height))
+font = pygame.font.SysFont(None, 25)
 pygame.display.update()
 pygame.display.set_caption('Simple Snake')
 
@@ -8,12 +13,18 @@ black = (0, 0, 0)
 blue=(0,0,255)
 
 x=100
-y=100
+y=150
 
 constantChangesX=0
 constantChangesY=0
 
 clock = pygame.time.Clock()
+
+font_style = pygame.font.SysFont(None, 50)
+ 
+def message(msg, color, x, y):
+    msg = font_style.render(msg, True, color)
+    game.blit(msg, [x, y])
 
 game_over=False
 while not game_over:
@@ -25,28 +36,54 @@ while not game_over:
 
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_UP:
-        constantChangesY = -5
-        constantChangesX = 0
+        if not constantChangesY == 10:
+          constantChangesY = -10
+          constantChangesX = 0
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_DOWN:
-        constantChangesY = 5
-        constantChangesX = 0
+        if not constantChangesY == -10:
+          constantChangesY = 10
+          constantChangesX = 0
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_LEFT:
-        constantChangesX = -5
-        constantChangesY = 0
+        if not constantChangesX == 10:
+          constantChangesX = -10
+          constantChangesY = 0
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_RIGHT:
-        constantChangesX = 5
-        constantChangesY = 0
+        if not constantChangesX == -10:
+          constantChangesX = 10
+          constantChangesY = 0
 
   y+=constantChangesY
   x+=constantChangesX
 
-  dis.fill(black)
-  pygame.draw.rect(dis,blue,[x,y,10,10])
+  if x > 295:
+    x = 0
+    constantChangesX = 10
+    constantChangesY = 0
+
+  if x < 0:
+    x = 290
+    constantChangesX = -10
+    constantChangesY = 0
+
+  if y > 295:
+    y = 0
+    constantChangesX = 0
+    constantChangesY = 10
+
+  if y < 0:
+    y = 290
+    constantChangesX = 0
+    constantChangesY = -10
+
+  game.fill(black)
+  pygame.draw.rect(game,blue,[x, y, 7, 7])
+  message("hi", blue, 10, 10)
   pygame.display.update()
-  clock.tick(10)
+  clock.tick(20)
+  print(x, y)
 
 # Food
   # if x = foodx
