d432ae7561c4e0fc8f9c3b09a961edb8124971e2
SuperJu5tin <superjuslib@gmail.com>
2022-09-21 13:37:30 -0600

    started react

--- react-snake/src/Snake.jsx
+++ react-snake/src/Snake.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export default function Snake() {
+
+  let Gameboard = {
+    x:{
+      color:"hi2"
+    }
+  }
+
   return (
-    <h1>Snake</h1>
+    <>
+      <h1>Snake</h1>
+      <h2>{Gameboard.x.color}</h2>
+    </>
   )
 }
